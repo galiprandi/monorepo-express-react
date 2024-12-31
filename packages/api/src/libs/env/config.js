09f7db517f8940b38b80e97ec0a24d0d8527602b
Germán Aliprandi <galiprandi@gmail.com>
2024-12-31 10:46:04 -0300

    Add configuration and logger modules for API

---
+++ packages/api/src/libs/env/config.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const EXTERNAL_API_URL = process.env.EXTERNAL_API_URL || 'https://echo-serv.tbxnet.com/v1/secret';
+export const API_KEY = process.env.API_KEY || 'Bearer aSuperSecretKey';
+export const PORT = process.env.PORT || 3000;
