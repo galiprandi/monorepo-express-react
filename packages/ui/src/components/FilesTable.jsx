a25b58600a811304f7cea7707ec6ad87cff101ac
Germán Aliprandi <galiprandi@gmail.com>
2025-01-01 19:04:04 -0300

    Enhance FilesTable component with search suggestions and improved layout

--- packages/ui/src/components/FilesTable.jsx
+++ packages/ui/src/components/FilesTable.jsx
@@ -1,11 +1,12 @@
 import { useFilesData } from '../hooks/useFilesData'
 import { Table } from './Table'
 import Form from 'react-bootstrap/Form'
+import styles from './FilesTable.module.css'
 
 export const FilesTable = () => {
   const urlParams = new URLSearchParams(window.location.search)
   const fileName = urlParams.get('fileName') || ''
-  const { records, status } = useFilesData(fileName)
+  const { records, fileList, status } = useFilesData(fileName)
 
   const props = {
     columns: ['File Name', 'Text', 'Number', 'Hex'],
@@ -19,21 +20,34 @@ export const FilesTable = () => {
     const searchParams = new URLSearchParams({ fileName })
     window.location.search = searchParams.toString()
   }
-  const noDataAvailable = !records?.length || status === 'error'
+
+  const noDataAvailable = status !== 'loading' && (!records?.length || status === 'error')
 
   return (
     <>
-      <Form onSubmit={handleSubmit}>
-        <input type='text' name='fileName' id='fileName' placeholder='Search by file name' defaultValue={fileName} />
-        <button type='submit'>Search</button>
-        {/* reset form button */}
-        <button type='button' onClick={() => { window.location.search = '' }}>Show All Files</button>
-      </Form>
+      <section className={styles.search}>
 
-      <br />
-      {status === 'loading' && <p>Loading...</p>}
-      {noDataAvailable && <p>No data available</p>}
-      <Table {...props} />
+        <Form onSubmit={handleSubmit}>
+          <input type='text' name='fileName' id='fileName' placeholder='Search by file name' defaultValue={fileName} list='fileList' />
+          {
+            fileList.length > 1 &&
+            <datalist id='fileList'>
+              {fileList.map((fileName, index) => (
+                <option key={index} value={fileName} />
+              ))}
+            </datalist>}
+          <button type='submit'>Search</button>
+          {/* reset form button */}
+          <button type='button' onClick={() => { window.location.search = '' }}>Show All Files</button>
+        </Form>
+      </section>
+
+      <section className={styles.table}>
+
+        {status === 'loading' && <p>Loading...</p>}
+        {noDataAvailable && <p>No data available</p>}
+        <Table {...props} />
+      </section>
     </>
   )
 }
