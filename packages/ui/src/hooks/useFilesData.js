db3d3c08c0578c94e1d6a354e865dfae9464408c
Germán Aliprandi <galiprandi@gmail.com>
2025-01-01 19:04:00 -0300

    Add fileList state to useFilesData hook for unique file names extraction

--- packages/ui/src/hooks/useFilesData.js
+++ packages/ui/src/hooks/useFilesData.js
@@ -7,41 +7,44 @@ import { API_URL } from '../libs/env/config'
  * @returns {Object} An object containing the table data, the status of the fetch request, and any error message.
  */
 export const useFilesData = (fileName = null) => {
-  console.log('fileName:', fileName)
-  const [rawData, setRawData] = useState([])
-  const [records, setRecords] = useState([])
-  const [status, setStatus] = useState('idle') // 'idle', 'loading', 'success', 'error'
-  const [error, setError] = useState(null)
+    const [rawData, setRawData] = useState([])
+    const [records, setRecords] = useState([])
+    const [fileList, setFileList] = useState([])
+    const [status, setStatus] = useState('idle') // 'idle', 'loading', 'success', 'error'
+    const [error, setError] = useState(null)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setStatus('loading')
-      setError(null)
-      try {
-        const baseUrl = new URL(`${API_URL}/files/data`)
-        if (fileName) baseUrl.searchParams.append('fileName', fileName)
 
-        const response = await fetch(baseUrl)
+    useEffect(() => {
+        const fetchData = async () => {
+            setStatus('loading')
+            setError(null)
+            try {
+                const baseUrl = new URL(`${API_URL}/files/data`)
+                if (fileName) baseUrl.searchParams.append('fileName', fileName)
 
-        if (!response.ok) { throw new Error('Network response was not ok') }
+                const response = await fetch(baseUrl)
 
-        const rawData = await response.json()
-        setRawData(rawData)
-        const records = rawData.flatMap(({ lines }) =>
-          lines.map((line) => Object.values(line))
-        )
+                if (!response.ok) { throw new Error('Network response was not ok') }
 
-        setRecords(records)
-        setStatus('success')
-      } catch (error) {
-        console.error('Fetch error:', error)
-        setError(error.message)
-        setStatus('error')
-      }
-    }
+                const rawData = await response.json()
+                setRawData(rawData)
+                const records = rawData.flatMap(({ lines }) =>
+                    lines.map((line) => Object.values(line))
+                )
+                const fileList = Array.from(new Set(records.map(([fileName]) => fileName)))
 
-    fetchData()
-  }, [])
+                setFileList(fileList)
+                setRecords(records)
+                setStatus('success')
+            } catch (error) {
+                console.error('Fetch error:', error)
+                setError(error.message)
+                setStatus('error')
+            }
+        }
 
-  return { records, rawData, status, error }
+        fetchData()
+    }, [])
+
+    return { records, fileList, rawData, status, error }
 }
