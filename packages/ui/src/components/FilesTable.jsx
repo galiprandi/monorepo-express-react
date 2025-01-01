e7d42bbac9d974b3832ae0d273d103f1fbc31e30
Germán Aliprandi <galiprandi@gmail.com>
2025-01-01 13:45:18 -0300

    Refactor useFilesData hook to separate raw data and processed records

--- packages/ui/src/components/FilesTable.jsx
+++ packages/ui/src/components/FilesTable.jsx
@@ -2,16 +2,19 @@ import { useFilesData } from '../hooks/useFilesData'
 import { Table } from './Table'
 
 export const FilesTable = () => {
-  const { data, status, error } = useFilesData()
+  const { records, status, error } = useFilesData()
 
   const props = {
     columns: ['File Name', 'Text', 'Number', 'Hex'],
-    data
+    data: records
   }
+
 
   if (status === 'loading') return <p>Loading...</p>
   if (status === 'error') return <p>Error: {error}</p>
-  if (!data || data.length === 0) return <p>No data available</p>
+  if (!records || records.length === 0) return <p>No data available</p>
 
   return <Table {...props} />
 }
+
+
--- packages/ui/src/hooks/useFilesData.js
+++ packages/ui/src/hooks/useFilesData.js
@@ -6,31 +6,39 @@ import { useState, useEffect } from 'react'
  * @returns {Object} An object containing the table data, the status of the fetch request, and any error message.
  */
 export const useFilesData = () => {
-  const [data, setData] = useState([])
-  const [status, setStatus] = useState('idle') // 'idle', 'loading', 'success', 'error'
-  const [error, setError] = useState(null)
+    const [rawData, setRawData] = useState([])
+    const [records, setRecords] = useState([])
+    const [status, setStatus] = useState('idle') // 'idle', 'loading', 'success', 'error'
+    const [error, setError] = useState(null)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setStatus('loading')
-      setError(null)
-      try {
-        const response = await fetch('http://localhost:3000/files/data')
-        if (!response.ok) {
-          throw new Error('Network response was not ok')
+    useEffect(() => {
+        const fetchData = async () => {
+            setStatus('loading')
+            setError(null)
+            try {
+                const response = await fetch('http://localhost:3000/files/data')
+                if (!response.ok) {
+                    throw new Error('Network response was not ok')
+                }
+                const rawData = await response.json()
+                setRawData(rawData)
+                const records = rawData.flatMap(({ lines }) =>
+                    lines.map((line) => Object.values(line))
+                )
+
+                setRecords(records)
+                setStatus('success')
+            } catch (error) {
+                console.error('Fetch error:', error)
+                setError(error.message)
+                setStatus('error')
+            }
         }
-        const result = await response.json()
-        setData(result)
-        setStatus('success')
-      } catch (error) {
-        console.error('Fetch error:', error)
-        setError(error.message)
-        setStatus('error')
-      }
-    }
 
-    fetchData()
-  }, [])
+        fetchData()
+    }, [])
 
-  return { data, status, error }
+    return { records, rawData, status, error }
 }
+
+
